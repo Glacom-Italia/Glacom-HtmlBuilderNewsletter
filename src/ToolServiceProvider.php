ffddd2249c891bf1de51e2631d23d19da859c93c
mattiaglacom <mattia@glacomtech.com>
2023-10-05 15:19:23 +0200

    first commit

---
+++ src/ToolServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Glacom\HtmlBuilderNewsletter;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Http\Middleware\Authenticate;
+use Laravel\Nova\Nova;
+use Glacom\HtmlBuilderNewsletter\Http\Middleware\Authorize;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //$this->loadViewsFrom(__DIR__.'/../resources/views', 'htmlbuilder_newsletter');
+
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            //
+        });
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+        Nova::router(['nova', Authenticate::class, Authorize::class], 'htmlbuildernewsletter')
+            ->group(__DIR__.'/../routes/inertia.php');
+
+        Route::middleware(['nova', Authorize::class])
+            ->prefix('nova-vendor/htmlbuildernewsletter')
+            ->group(__DIR__.'/../routes/api.php');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
